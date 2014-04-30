13733b300ba8ce7eeeff2b7284a8985a6dafe275
Mingzhi Zeng <zeostudio@gmail.com>
2014-04-30 11:42:57 +0800

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,19 @@
 sudo apt-get update
 sudo apt-get install python
 sudo apt-get install build-essential python-dev
+sudo apt-get install aptitude
 sudo aptitude install xvfb
 sudo aptitude install x11-xkb-utils
 sudo aptitude install xserver-xorg-core
 sudo aptitude install xfonts-100dpi xfonts-75dpi xfonts-scalable xfonts-cyrillic
-
-sudo apt-get install pip
+#wget http://download.redis.io/redis-stable.tar.gz
+#tar xvzf redis-stable.tar.gz
+#cd redis-stable
+#make
+sudo apt-get install redis-server
+sudo apt-get install python-lxml
+cd ..
+sudo apt-get install python-pip
 sudo pip install -r requirements.txt
 
 
@@ -19,6 +26,4 @@ sudo apt-get install google-chrome-stable
 wget -O /tmp/chromedriver.zip http://chromedriver.storage.googleapis.com/2.9/chromedriver_linux64.zip && sudo unzip /tmp/chromedriver.zip chromedriver -d /usr/local/bin/
 sudo chmod 777 /usr/local/bin/chromedriver
 
-
-sudo apt-get install python-scrapy
-sudo apt-get install redis
+mkdir log
